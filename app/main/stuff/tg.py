3f16b1d1bdb6a94ab805afd0109b738cb19ca63b
zasyp <zasypkin4514@gmail.com>
2024-05-25 20:05:47 +0300

    разбил по файлам

--- app/main/stuff/tg.py
+++ app/main/stuff/tg.py
@@ -1,7 +1,2 @@
-import telebot
-from base import init_db
-from handlers import bot  # Импортируем объект бота из stuff.py
-
-if __name__ == '__main__':
-    init_db()  # Инициализация базы данных
-    bot.infinity_polling()  # Запуск бота
+import asyncio
+import aiogram
