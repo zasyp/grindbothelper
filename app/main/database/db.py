e24f4fc82ea05917a8e6d272c5ec5fe5628eddee
zasyp <zasypkin4514@gmail.com>
2024-05-26 19:28:32 +0300

    кнопки дневника в одной строчке

--- app/main/database/db.py
+++ app/main/database/db.py
@@ -7,9 +7,7 @@ DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
-AsyncSessionLocal = sessionmaker(
-    engine, class_=AsyncSession, expire_on_commit=False
-)
+AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 async_session = AsyncSessionLocal()
 
